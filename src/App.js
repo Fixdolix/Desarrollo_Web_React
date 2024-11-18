efb4893f39e1b96d09f1d87b8bcdf29c38df775d
Fixdolix <f.florescastaer@uandresbello.edu>
2024-11-18 17:34:22 -0300

    Carrito y devolucion sujetos al nuevo clean

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'; 
+
+// Importa los componentes que quieres renderizar
+import Carrito from './carrito';
+import Devolucion from './devolucion';
+
+// Componente para el layout de la página principal
+function Layout({ message }) {
+  return (
+    <div>
+      <h1>Prueba de conexión</h1>
+      <p>{message}</p>
+
+      {/* Links para navegar entre páginas */}
+      <nav>
+        <ul>
+          <li>
+            <Link to="/carrito">Carrito</Link>
+          </li>
+          <li>
+            <Link to="/devolucion">Devolución</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
 
 function App() {
   const [message, setMessage] = useState('');
@@ -13,10 +40,14 @@ function App() {
   }, []);
 
   return (
-    <div>
-      <h1>Prueba de conexión</h1>
-      <p>{message}</p>
-    </div>
+    <Router>
+      {/* Ruta para la página principal con layout */}
+      <Routes>
+        <Route path="/" element={<Layout message={message} />} />
+        <Route path="/carrito" element={<Carrito />} />
+        <Route path="/devolucion" element={<Devolucion />} />
+      </Routes>
+    </Router>
   );
 }
 
